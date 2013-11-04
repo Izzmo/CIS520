a20f2d3511ec5739a2b2ef68544188b1eb69410e
Blake  Koblitz <bkoblitz@cougar.cis.ksu.edu>
2013-11-04 16:14:45 -0600

    added read, write, seek, tell

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -31,9 +31,21 @@ static int sys_close (int handle);
  
 static void syscall_handler (struct intr_frame *);
 static void copy_in (void *, const void *, size_t);
+
+static struct file *find_file_by_fd (int fd);
+static struct fd_elem *find_fd_elem_by_fd (int fd);
  
+struct fd_elem
+{
+  int fd;
+  struct file *file;
+  struct list_elem elem;
+  struct list_elem thread_elem;
+};
+
 /* Serializes file system operations. */
 static struct lock fs_lock;
+static struct list file_list;
  
 void
 syscall_init (void) 
@@ -229,82 +241,122 @@ sys_filesize (int handle)
  
 /* Read system call. */
 static int
-sys_read (int handle, void *udst_, unsigned size) 
+sys_read (int fd, void *buffer, unsigned size)
 {
-/* Add code */
-  thread_exit ();
-}
- 
-/* Write system call. */
-static int
-sys_write (int handle, void *usrc_, unsigned size) 
-{
-  uint8_t *usrc = usrc_;
-  struct file_descriptor *fd = NULL;
-  int bytes_written = 0;
-
-  /* Lookup up file descriptor. */
-  if (handle != STDOUT_FILENO)
-    fd = lookup_fd (handle);
-
+  struct file * f;
+  unsigned i;
+  int ret;
+  
+  ret = -1; /* Initialize to zero */
   lock_acquire (&fs_lock);
-  while (size > 0) 
+  if (fd == STDIN_FILENO) /* stdin */
     {
-      /* How much bytes to write to this page? */
-      size_t page_left = PGSIZE - pg_ofs (usrc);
-      size_t write_amt = size < page_left ? size : page_left;
-      off_t retval;
-
-      /* Check that we can touch this user page. */
-      if (!verify_user (usrc)) 
-        {
-          lock_release (&fs_lock);
-          thread_exit ();
-        }
-
-      /* Do the write. */
-      if (handle == STDOUT_FILENO)
-        {
-          putbuf (usrc, write_amt);
-          retval = write_amt;
-        }
-      else
-        retval = file_write (fd->file, usrc, write_amt);
-      if (retval < 0) 
-        {
-          if (bytes_written == 0)
-            bytes_written = -1;
-          break;
-        }
-      bytes_written += retval;
-
-      /* If it was a short write we're done. */
-      if (retval != (off_t) write_amt)
-        break;
-
-      /* Advance. */
-      usrc += retval;
-      size -= retval;
+      for (i = 0; i != size; ++i)
+        *(uint8_t *)(buffer + i) = input_getc ();
+      ret = size;
+      goto done;
     }
+  else if (fd == STDOUT_FILENO) /* stdout */
+      goto done;
+  else if (!is_user_vaddr (buffer) || !is_user_vaddr (buffer + size)) /* bad ptr */
+    {
+      lock_release (&fs_lock);
+      sys_exit (-1);
+    }
+  else
+    {
+      f = find_file_by_fd (fd);
+      if (!f)
+        goto done;
+      ret = file_read (f, buffer, size);
+    }
+    
+done:    
   lock_release (&fs_lock);
- 
-  return bytes_written;
+  return ret;
+}
+
+static struct file *
+find_file_by_fd (int fd)
+{
+  struct fd_elem *ret;
+  
+  ret = find_fd_elem_by_fd (fd);
+  if (!ret)
+    return NULL;
+  return ret->file;
+}
+
+static struct fd_elem *
+find_fd_elem_by_fd (int fd)
+{
+  struct fd_elem *ret;
+  struct list_elem *l;
+  
+  for (l = list_begin (&file_list); l != list_end (&file_list); l = list_next (l))
+    {
+      ret = list_entry (l, struct fd_elem, elem);
+      if (ret->fd == fd)
+        return ret;
+    }
+    
+  return NULL;
+}
+
+static int
+sys_write (int fd, void *buffer, unsigned length)
+{
+  struct file * f;
+  int ret;
+  
+  ret = -1;
+  lock_acquire (&fs_lock);
+  if (fd == STDOUT_FILENO) /* stdout */
+    putbuf (buffer, length);
+  else if (fd == STDIN_FILENO) /* stdin */
+    goto done;
+  else if (!is_user_vaddr (buffer) || !is_user_vaddr (buffer + length))
+    {
+      lock_release (&fs_lock);
+      sys_exit (-1);
+    }
+  else
+    {
+      f = find_file_by_fd (fd);
+      if (!f)
+        goto done;
+        
+      ret = file_write (f, buffer, length);
+    }
+    
+done:
+  lock_release (&fs_lock);
+  return ret;
 }
  
 /* Seek system call. */
 static int
-sys_seek (int handle, unsigned position) 
+sys_seek (int fd, unsigned pos)
 {
-/* Add code */
-  thread_exit ();
+  struct file *f;
+  
+  f = find_file_by_fd (fd);
+  if (!f)
+    return -1;
+  file_seek (f, pos);
+  return 0; /* Not used */
 }
  
 /* Tell system call. */
 static int
-sys_tell (int handle) 
+sys_tell (int fd)
 {
-/* Add code */
-  thread_exit ();
+  struct file *f;
+  
+  f = find_file_by_fd (fd);
+  if (!f)
+    return -1;
+  return file_tell (f);
 }
  
 /* Close system call. */
