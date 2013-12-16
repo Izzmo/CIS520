15a5e0902feabffe4598394ef5712a46dc723ecb
Tyler <tylerhutch21@gmail.com>
2013-12-16 15:10:02 -0600

    MarbleGame Done

--- Android/MarbleGame/gen/com/example/android/accelerometerplay/R.java
+++ Android/MarbleGame/gen/com/example/android/accelerometerplay/R.java
@@ -15,8 +15,14 @@ public final class R {
         public static final int icon=0x7f020001;
         public static final int wood=0x7f020002;
     }
+    public static final class id {
+        public static final int newgame=0x7f060000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int my_options=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
